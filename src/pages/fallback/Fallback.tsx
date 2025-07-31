f50c0a7dfde24198c545896d3f353f688a474831
wanse-dev <wansedev@gmail.com>
2025-07-31 03:41:57 -0300

    added button to go back to home page in fallback

--- src/pages/fallback/Fallback.tsx
+++ src/pages/fallback/Fallback.tsx
@@ -1,10 +1,13 @@
 import "./FallBack.css";
+import { Link } from "react-router";
 
 export const FallBack = () => {
   return (
     <section className="fall-back">
-      <p>fallback (404 not found)</p>
-      {/* TO-DO: traer el error por parámetro, asi lo muestro acá */}
+      <p>(404 not found)</p>
+      <Link className="go-back-button" to="/" draggable={false}>
+        Back to main page
+      </Link>
     </section>
   );
 };
